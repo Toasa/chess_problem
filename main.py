fea5dd63d9727d2c41a41912715eedeb8dc78452
toasa <atsushi.tohyama.160.333@gmail.com>
2019-01-29 21:40:07 +0900

    add: eval func

--- main.py
+++ main.py
@@ -1,25 +1,105 @@
 import chess.pgn
+import chess
 
-pgn = open("./data/auerswald.pgn")
+def get_value(board):
+    # material advantage
+    values = {chess.PAWN: 1,
+              chess.KNIGHT: 3,
+              chess.BISHOP: 3.3,
+              chess.ROOK: 5,
+              chess.QUEEN: 9,
+              chess.KING: 0}
 
-count = 1
-while 1:
-    game = chess.pgn.read_game(pgn)
+    val = 0.0
+    # piece values
+    pm = board.piece_map()
+    for x in pm:
+        tval = values[pm[x].piece_type]
+        if pm[x].color == chess.WHITE:
+            val += tval
+        else:
+            val -= tval
 
-    if game is None:
-        break
+    # add a number of legal moves term
+    bak = board.turn
+    board.turn = chess.WHITE
+    val += 0.1 * board.legal_moves.count()
+    board.turn = chess.BLACK
+    val -= 0.1 * board.legal_moves.count()
+    board.turn = bak
 
-    board = game.board()
-    print("first board")
-    print(board)
-    input()
+    # val of center board
 
-    for move in game.mainline_moves():
-        board.push(move)
-        print(move, count)
-        print(board)
-        input()
-        count += 1
+    # king position(defensive of kings)
 
-    print("end\n")
+    # available castling?
+
+    # connected of pieces、紐チェック
+
+    return val
+
+def print_init_position():
+    print("a b c d e f g h ")
+    print("                8")
+    print("                7")
+    print("                6")
+    print("                5")
+    print("                4")
+    print("                3")
+    print("                2")
+    print("                1")
+
+def main():
+    pgn = open("./data/auerswald.pgn")
+
+    print_init_position()
+
     count = 1
+    while 1:
+        game = chess.pgn.read_game(pgn)
+
+        if game is None:
+            break
+
+        board = game.board()
+        print("value:", round(get_value(board), 4))
+        print(board)
+        print("s: start, n: next game, q: quit => ", end = "")
+
+        i = input()
+        if i == "s":
+            pass
+        elif i == "n":
+            continue
+        elif i == "q":
+            break
+
+        for move in game.mainline_moves():
+            print("move => ", end = "")
+            move_player = input()
+
+
+            val = get_value(board)
+
+            if move.uci() == move_player:
+                print("correct!", move, "\nvalue:", round(val, 4))
+            else:
+                print("false", move, "\nvalue:", round(val, 4))
+
+            board.push(move)
+            print(count)
+            print(board)
+            count += 1
+
+        print("end\n")
+        i = input()
+        if i == "s":
+            pass
+        elif i == "n":
+            continue
+        elif i == "q":
+            break
+        count = 1
+
+if __name__ == "__main__":
+    main()
